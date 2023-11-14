2f2eb57b076ebf3ab265e77a021111a5a7cec175
Srking501 <alshadadiabdullah853@gmail.com>
2023-11-14 19:41:51 +0000

    completed the data exclusion part

--- munge/01-A.R
+++ munge/01-A.R
@@ -1,1 +1,24 @@
-# Example preprocessing script.
+# Data Inclusion / Exclusion
+# Keeps `learner_id`, `quiz_question` and `correct` columns and removes
+# the rest
+run1_qr = cyber.security.1_question.response %>% select(learner_id,
+                                                        quiz_question,
+                                                        correct)
+run2_qr = cyber.security.2_question.response %>% select(learner_id,
+                                                        quiz_question,
+                                                        correct)
+run3_qr = cyber.security.3_question.response %>% select(learner_id,
+                                                        quiz_question,
+                                                        correct)
+run4_qr = cyber.security.4_question.response %>% select(learner_id,
+                                                        quiz_question,
+                                                        correct)
+run5_qr = cyber.security.5_question.response %>% select(learner_id,
+                                                        quiz_question,
+                                                        correct)
+run6_qr = cyber.security.6_question.response %>% select(learner_id,
+                                                        quiz_question,
+                                                        correct)
+run7_qr = cyber.security.7_question.response %>% select(learner_id,
+                                                        quiz_question,
+                                                        correct)
